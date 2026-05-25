5ad033d2ce2b7070a3abc0e7fefc74eb0328f3d7
Mai Wael <100417213+MaiWael25@users.noreply.github.com>
2026-05-25 16:26:36 +0300

    Update dockerclass.groovy

--- src/edu/iti/dockerclass.groovy
+++ src/edu/iti/dockerclass.groovy
@@ -1,3 +1,4 @@
+/*
 package edu.iti;
 
 def build(imageName, imageTag)
@@ -16,4 +17,26 @@ def login(userName, passWord)
     sh "docker login -u ${userName} -p ${passWord}"
 
 }
+*/
+package edu.iti
 
+class DockerClass implements Serializable {
+
+    def steps
+
+    DockerClass(steps) {
+        this.steps = steps
+    }
+
+    def build(imageName, imageTag) {
+        steps.sh "docker build -t ${imageName}:${imageTag} ."
+    }
+
+    def push(imageName, imageTag) {
+        steps.sh "docker push ${imageName}:${imageTag}"
+    }
+
+    def login(userName, passWord) {
+        steps.sh "docker login -u ${userName} -p ${passWord}"
+    }
+}
