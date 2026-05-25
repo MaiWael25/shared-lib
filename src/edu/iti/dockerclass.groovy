8fb41cd0241500376aa878c99e669734f7d080ba
Mai Wael <maiw8757@gmail.com>
2026-05-25 15:50:14 +0300

    comment push

--- src/edu/iti/dockerclass.groovy
+++ src/edu/iti/dockerclass.groovy
@@ -5,11 +5,12 @@ def build(imageName, imageTag)
     sh "docker build -t ${imageName}:${imageTag}"
 
 }
-def push(imageName, imageTag)
+/*def push(imageName, imageTag)
 {
     sh "docker push -t ${imageName}:${imageTag}"
 
 }
+*/
 def login(userName, passWord)
 {
     sh "docker login -u ${userName} -p ${passWord}"
